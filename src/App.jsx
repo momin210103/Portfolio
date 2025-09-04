feae39665a49a74500d3f670efd6b41e0bc74a37
momin210103 <momincse13@gmail.com>
2025-09-04 12:10:38 +0600

    first

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import NotFound from "./pages/NotFound.jsx";
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
